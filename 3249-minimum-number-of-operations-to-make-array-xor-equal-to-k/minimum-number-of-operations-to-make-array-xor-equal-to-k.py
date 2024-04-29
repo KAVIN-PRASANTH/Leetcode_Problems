acce08211bee2e6cf259f490c4d758d08f86a3fd
S KAVIN PRASANTH <123450555+KAVIN-PRASANTH@users.noreply.github.com>
2024-04-29 21:03:43 +0530

    Time: 587 ms (50.32%) | Memory: 31.2 MB (15.29%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -1,27 +1,8 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        c=0
-        res=str(bin(k)).replace('b','')
-        a=list(map(lambda e:str(e).replace('b',''),list(map(bin,nums))))
-        maxlen=max(list(map(lambda e:len(e),a)))
-        if len(res)>maxlen:
-            maxlen=len(res)
-        def eq(e):
-            e=str(e)
-            if len(e)!=maxlen:
-                ex="0"*(maxlen-len(e))
-                return ex+e
-            return e
-        a=list(map(eq,a))
-        if len(res)!=maxlen:
-            res=eq(res)
-        for i in range(0,maxlen):
-            count=0
-            xor=0
-            for k in a:
-                xor^=int(k[i])
-            if int(res[i])!=int(xor):
-                c+=1
-        return c
-
+        dum=0
+        for i in nums:
+            dum^=i
+        dum^=k
+        return bin(dum).count("1")
         
