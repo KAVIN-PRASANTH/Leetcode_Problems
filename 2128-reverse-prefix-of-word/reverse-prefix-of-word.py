b6abc2b47af03a22e1907c3e808a9e61f794ec08
S KAVIN PRASANTH <123450555+KAVIN-PRASANTH@users.noreply.github.com>
2024-05-01 08:15:38 +0530

    Time: 30 ms (85.15%) | Memory: 16.6 MB (9.30%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -4,9 +4,6 @@ class Solution:
         for i in range(0,len(word)):
             if word[i]==ch:
                 break
-        print(i)
-        print(word,word[1:len(word)])
-        print(word[i::-1])
         if i+1==len(word) and word[i]!=ch:
             return word
         return word[i::-1]+word[i+1:len(word)]
