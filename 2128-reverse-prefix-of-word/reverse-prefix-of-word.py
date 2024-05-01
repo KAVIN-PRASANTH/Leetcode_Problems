63222bb04a27026594523208477d22907e329203
S KAVIN PRASANTH <123450555+KAVIN-PRASANTH@users.noreply.github.com>
2024-05-01 08:19:03 +0530

    Time: 42 ms (10.40%) | Memory: 16.5 MB (46.70%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -1,11 +1,8 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        j,i=0,0
-        for i in range(0,len(word)):
-            if word[i]==ch:
-                break
-        if i+1==len(word) and word[i]!=ch:
+        a=word.find(ch)
+        if a==-1:
             return word
-        return word[i::-1]+word[i+1:len(word)]
+        return word[a::-1]+word[a+1:len(word)]
 
         
