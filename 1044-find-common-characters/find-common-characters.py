588ab56a4cab66539b8cccca18c4d0f15ee60833
S KAVIN PRASANTH <123450555+KAVIN-PRASANTH@users.noreply.github.com>
2024-06-05 19:27:12 +0530

    Time: 61 ms (10.09%) | Memory: 16.8 MB (29.50%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.py
@@ -0,0 +1,33 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        resmap={}
+        final=[]
+        for i in words[0]:
+            if i in resmap:
+                resmap[i]+=1
+            else:
+                resmap[i]=1
+        words.pop(0)
+        print(resmap,words)
+        for i in words:
+            check={}
+            for j in i:
+                if j in check:
+                    check[j]+=1
+                else:
+                    check[j]=1
+            for key,value in resmap.items():
+                if key in check:
+                    resmap[key]=min(check[key],value)
+                else:
+                    resmap[key]=0
+            print("-->",check,resmap)
+        for key,value in resmap.items():
+            for index in range(0,value):
+                final.append(key)
+        return final
+
+                
+            
+
+        
