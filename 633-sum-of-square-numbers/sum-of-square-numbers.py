a5b639cda8e627abc79b66aa4747a3b1b1b1d275
S KAVIN PRASANTH <123450555+KAVIN-PRASANTH@users.noreply.github.com>
2024-06-17 14:19:48 +0530

    Time: 714 ms (5.00%) | Memory: 16.5 MB (79.87%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        for i in range(0,(c//2)+2):
+            cal=c-(i*i)
+
+            print(i,cal)
+            if cal<0:
+                return False
+            cal=math.sqrt(cal)
+            cal=str(cal).split(".")
+            cal=int(cal[1])
+            if cal==0:
+                return True
+        return False
+    
+        
+       
+        
